8af5e00ac0af8e061d0f27611e888a5f254cf6e7
JoefsMaggie <joe_fs@sina.com>
2019-03-07 03:14:10 +0000

    Update CollectionHelper.java

--- basic/core/src/main/java/joe/core/utils/CollectionHelper.java
+++ basic/core/src/main/java/joe/core/utils/CollectionHelper.java
@@ -1,15 +1,15 @@
 package joe.core.utils;
 
 public class CollectionHelper {
-    public <T> Map<String, T> listMap2Map(List<Map<String, Object>> list, String key, String value) {
+    public <V> Map<String, V> listMap2Map(List<Map<String, Object>> list, String key, String value) {
         return list.parallelStream()
-                .map(m -> Collections.singletonMap(m.get(key).toString(), (T) m.get(value)))
+                .map(m -> Collections.singletonMap(m.get(key).toString(), (V) m.get(value)))
                 .map(Map::entrySet)
                 .flatMap(Set::stream)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    public <T> Map<String, T> listMap2Map(List<Map<String, Object>> list, String key, Function<Map<String, Object>, T> valFun) {
+    public <V> Map<String, V> listMap2Map(List<Map<String, Object>> list, String key, Function<Map<String, Object>, V> valFun) {
         return list.parallelStream()
                 .map(m -> Collections.singletonMap(m.get(key).toString(), valFun.apply(m)))
                 .map(Map::entrySet)
