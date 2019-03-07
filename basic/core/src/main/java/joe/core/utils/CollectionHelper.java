c1c4168657d5fe75df4b05054125d1dbd8ba9b5e
JoefsMaggie <joe_fs@sina.com>
2019-03-07 03:24:17 +0000

    Update CollectionHelper.java

--- basic/core/src/main/java/joe/core/utils/CollectionHelper.java
+++ basic/core/src/main/java/joe/core/utils/CollectionHelper.java
@@ -1,7 +1,7 @@
 package joe.core.utils;
 
 public class CollectionHelper {
-    public <V> Map<String, V> listMap2Map(List<Map<String, Object>> list, String key, String value) {
+    public <V, T> Map<String, V> listMap2Map(List<Map<String, T>> list, String key, String value) {
         return list.parallelStream()
                 .map(m -> Collections.singletonMap(m.get(key).toString(), (V) m.get(value)))
                 .map(Map::entrySet)
@@ -9,7 +9,7 @@ public class CollectionHelper {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    public <V> Map<String, V> listMap2Map(List<Map<String, Object>> list, String key, Function<Map<String, Object>, V> valFun) {
+    public <V, T> Map<String, V> listMap2Map(List<Map<String, T>> list, String key, Function<Map<String, T>, V> valFun) {
         return list.parallelStream()
                 .map(m -> Collections.singletonMap(m.get(key).toString(), valFun.apply(m)))
                 .map(Map::entrySet)
@@ -17,7 +17,7 @@ public class CollectionHelper {
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
     
-    public <K, V> Map<K, V> listMap2Map(List<Map<String, Object>> list, Function<Map<String, Object>, K> keyFun, Function<Map<String, Object>, V> valFun) {
+    public <K, V, T> Map<K, V> listMap2Map(List<Map<String, T>> list, Function<Map<String, T>, K> keyFun, Function<Map<String, T>, V> valFun) {
         return list.parallelStream()
                 .map(m -> Collections.singletonMap(keyFun.apply(m), valFun.apply(m)))
                 .map(Map::entrySet)
